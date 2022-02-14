82f3cd66ac83cb6b351ab3d310a3e9b4e1165b35
Aleksei Zhitenev <leikoplasterr@gmail.com>
2022-02-14 13:48:57 +0000

    Update sqlusers.sql

---
+++ sqlusers.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users
+(
+    id   SERIAL PRIMARY KEY,
+    email VARCHAR (100) NOT NULL,
+    full_name VARCHAR (100) NOT NULL,
+    password VARCHAR (20) NOT NULL
+)
+
+CREATE TABLE role
+(
+    id   SERIAL PRIMARY KEY,
+    name varchar(100) not null
+)
+
+CREATE TABLE user_role
+(
+    user_id INT NOT NULL REFERENCES users (id),
+    role_id INT NOT NULL REFERENCES role (id)
+)
