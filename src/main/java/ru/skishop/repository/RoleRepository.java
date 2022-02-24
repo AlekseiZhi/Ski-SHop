1f7ac47c0944744c2b5c72230c65be0233f6b947
Aleksei <92666977+AlekseiZhi@users.noreply.github.com>
2022-02-24 18:48:19 +0300

    ignore iml

--- src/main/java/ru/skishop/repository/RoleRepository.java
+++ src/main/java/ru/skishop/repository/RoleRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.skishop.entities.Role;
+
 import java.util.List;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
