6fc7e56c12fd33fc7569cbed671012deb44ad1d1
Aleksei <92666977+AlekseiZhi@users.noreply.github.com>
2022-03-16 12:38:02 +0300

    create sql for create admin

--- src/main/resources/sqlusers.sql
+++ src/main/resources/sqlusers.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users
 (
-    id   SERIAL PRIMARY KEY,
-    email VARCHAR (100) UNIQUE NOT NULL,
-    full_name VARCHAR (100) NOT NULL,
-    password VARCHAR (100) NOT NULL
+    id        SERIAL PRIMARY KEY,
+    email     VARCHAR(100) UNIQUE NOT NULL,
+    full_name VARCHAR(100)        NOT NULL,
+    password  VARCHAR(100)        NOT NULL
 );
 
 CREATE TABLE role
@@ -17,3 +17,9 @@ CREATE TABLE user_role
     user_id INT NOT NULL REFERENCES users (id),
     role_id INT NOT NULL REFERENCES role (id)
 );
+
+INSERT INTO users (id, email, full_name, password)
+VALUES (1, 'admin@mail', 'admin', '$2a$10$thwp32FYaBkCI9.zaJW.ReR90HTPywIveWoApdjA/.2NTmuMjaGoi');
+
+INSERT INTO user_role (user_id, role_id)
+VALUES (1, 1);
