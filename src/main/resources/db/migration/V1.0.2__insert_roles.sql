e54975df0b61514990903f0277c78d3ac0a371bf
Aleksei <92666977+AlekseiZhi@users.noreply.github.com>
2022-03-16 16:31:43 +0300

    clean roles

--- src/main/resources/db/migration/V1.0.2__insert_roles.sql
+++ src/main/resources/db/migration/V1.0.2__insert_roles.sql
@@ -1,5 +1,2 @@
 INSERT INTO role (id, name ) VALUES (1, 'admin');
 INSERT INTO role (id, name ) VALUES (2, 'user');
-INSERT INTO role (id, name ) VALUES (3, 'user1');
-INSERT INTO role (id, name ) VALUES (4, 'user2');
-INSERT INTO role (id, name ) VALUES (5, 'user3');
