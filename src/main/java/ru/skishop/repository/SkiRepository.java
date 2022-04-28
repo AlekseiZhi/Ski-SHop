ad703d53a87aef233fe644e677cc485bc95ab604
AlekseiZhi <92666977+AlekseiZhi@users.noreply.github.com>
2022-04-28 13:26:56 +0300

    clean_code

--- src/main/java/ru/skishop/repository/SkiRepository.java
+++ src/main/java/ru/skishop/repository/SkiRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.skishop.entity.Ski;
 
 public interface SkiRepository extends JpaRepository<Ski, Long> {
+
     Ski findSkisById(Long id);
+
 }
